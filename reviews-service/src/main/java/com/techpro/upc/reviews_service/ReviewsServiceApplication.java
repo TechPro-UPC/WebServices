05c9bc9fab18cff7d9af62fc32f8b6085621b9df
Nicolini03 <U202218791@upc.edu.pe>
2025-10-31 00:10:58 -0500

    feat(auditing): Enable JPA Auditing for ReviewsServiceApplication

--- reviews-service/src/main/java/com/techpro/upc/reviews_service/ReviewsServiceApplication.java
+++ reviews-service/src/main/java/com/techpro/upc/reviews_service/ReviewsServiceApplication.java
@@ -2,8 +2,10 @@ package com.techpro.upc.reviews_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ReviewsServiceApplication {
 
 	public static void main(String[] args) {

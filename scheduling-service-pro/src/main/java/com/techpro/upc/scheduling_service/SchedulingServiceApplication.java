82d0dc3cb430bc7c07743029b9b36e521caab423
iconicmiau <u202224135@gmail.com>
2025-11-01 17:25:36 -0500

    feat: add eureka service for scheduling

--- scheduling-service-pro/src/main/java/com/techpro/upc/scheduling_service/SchedulingServiceApplication.java
+++ scheduling-service-pro/src/main/java/com/techpro/upc/scheduling_service/SchedulingServiceApplication.java
@@ -3,9 +3,11 @@ package com.techpro.upc.scheduling_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients // habilita el escaneo de @FeignClient en este proyecto
 public class SchedulingServiceApplication {
     public static void main(String[] args) {

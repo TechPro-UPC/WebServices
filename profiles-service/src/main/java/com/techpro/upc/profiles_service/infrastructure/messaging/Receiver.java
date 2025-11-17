f1309c2f7bd89108abda71cf2d51fea54cae2bcc
iconicmiau <u202224135@gmail.com>
2025-11-16 20:17:38 -0500

    feat: try rabbitmq for the first time

---
+++ profiles-service/src/main/java/com/techpro/upc/profiles_service/infrastructure/messaging/Receiver.java
@@ -0,0 +1,19 @@
+package com.techpro.upc.profiles_service.infrastructure.messaging;
+
+import java.util.concurrent.CountDownLatch;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+    private final CountDownLatch latch = new CountDownLatch(1);
+
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+}

b83711536e9d5d7b51f16227ae2d00047ecd5c30
Nicolini03 <U202218791@upc.edu.pe>
2025-12-01 18:10:16 -0500

    chore(bootstrap): update iam service application bootstrap

--- iam-service/src/main/java/com/techpro/upc/iam_service/IamServiceApplication.java
+++ iam-service/src/main/java/com/techpro/upc/iam_service/IamServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAsync
 public class IamServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(IamServiceApplication.class, args);

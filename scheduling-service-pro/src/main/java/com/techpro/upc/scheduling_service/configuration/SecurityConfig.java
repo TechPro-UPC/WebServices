6647fc5f59ee07b65b04f0e31a67dbd719c34b6d
Nicolini03 <U202218791@upc.edu.pe>
2025-12-02 18:59:18 -0500

    feat(scheduling): setup service application and security config

--- scheduling-service-pro/src/main/java/com/techpro/upc/scheduling_service/configuration/SecurityConfig.java
+++ scheduling-service-pro/src/main/java/com/techpro/upc/scheduling_service/configuration/SecurityConfig.java
@@ -3,20 +3,22 @@ package com.techpro.upc.scheduling_service.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable) // Necesario para que funcionen los POST
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs.yaml").authenticated()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(basic -> {}); // pide user/pass
+                        // PERMITIRTODO (Ideal para desarrollo rápido)
+                        .anyRequest().permitAll()
+                );
+
         return http.build();
     }
 }

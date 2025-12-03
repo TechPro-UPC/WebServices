f639d4d9ea09b505a2cf4847348a53ec967443b1
Rafooo6 <rafaelbravo032@gmail.com>
2025-12-02 21:14:57 -0500

    feat: update

--- catalog-service/src/main/java/com/techpro/upc/catalog_service/infrastructure/scheduling/SchedulingClient.java
+++ catalog-service/src/main/java/com/techpro/upc/catalog_service/infrastructure/scheduling/SchedulingClient.java
@@ -1,18 +1,12 @@
 package com.techpro.upc.catalog_service.infrastructure.scheduling;
 
 
-import com.techpro.upc.catalog_service.infrastructure.configuration.FeignClientInterceptor;
 import com.techpro.upc.catalog_service.infrastructure.scheduling.resources.TimeSlotResource;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(
-        name = "scheduling-service",
-        url = "${scheduling-service.url}",
-        configuration = FeignClientInterceptor.class
-)
+
 public interface SchedulingClient {
 
     @GetMapping("/api/v1/time-slots")
